f30f4db176a554b324836919464badd6cff01941
Musooo <nicoloblack2@gmail.com>
2024-07-02 15:46:44 +0200

    coin generation implemented, position checkers done

--- src/mapHandler.go
+++ src/mapHandler.go
@@ -1,21 +1,62 @@
 package main
 
-func PrintMap(){
+import (
+	"math/rand"
+	"fmt"
+)
 
+func PrintMap(f Field) {
+	fmt.Print("\033[H\033[2J")
+	for _, row := range f.matrix {
+        for _, cell := range row {
+            fmt.Print(cell)
+        }
+        fmt.Println()
+    }
 }
 
-func GenerateCoin(){
-
+func PositionPlayer(f *Field, p Player){
+    f.matrix[p.y][p.x] = " @ "
 }
 
-func IsCoinTouched(){
-
+func UpdatePlayerPos(p *Player, f *Field, x, y int){
+    switch x{
+        case 0:
+            fallthrough
+        case 6:
+            break
+        default:
+            f.matrix[p.y][p.x] = "   "
+            p.x = x
+            PositionPlayer(f, *p)
+    }
+    switch y{
+        case 0:
+            fallthrough
+        case 8:
+            break
+        default:
+            f.matrix[p.y][p.x] = "   "
+            p.y = y
+            PositionPlayer(f, *p)
+    }
 }
 
-func CanPlayerGo(){
+func GenerateCoin(c *Coin, f *Field, p Player) {
+    c.x = 1 + rand.Intn(5-1)
+    c.y = 1 + rand.Intn(7-1)
+    //check if the pos is the same as the player
 
-}//if the player cannot move in that pos it stays there
+    if(isCoinTouched(*c, p)){
+        GenerateCoin(c, f, p)
+    }else {
+        f.matrix[c.y][c.x] = " 0 "
+    }
+}
 
-func CheckIfCoinPosIsAcceptable(){
-
-}//this should be used here, check if the player and the coin pos do not collide
+func isCoinTouched(c Coin, p Player) bool{
+    if(c.x == p.x && c.y == p.y){
+        return true
+    }
+    return false
+}
